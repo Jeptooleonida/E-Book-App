432c32910d224e51bc5de12dc2f4f32d77643045
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-07-28 23:03:35 +0300

    create a footer section

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Styles/Footer.css";
+
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p> E-Tech Collection @ 2022&copy;</p>
+    </div>
+  );
+};
+
+export default Footer;
