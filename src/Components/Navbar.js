a20fc437318b4ba94023dc70fd0415a2f98a9071
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-07-28 19:54:51 +0300

    create a Navbar Component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-header">
+                <h1>E-Tech App</h1>
+            </div>
+            <div className="navbar-content">
+                <Link to="/favorites">
+                    <h3>Your favorites</h3>
+                </Link>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Navbar;
