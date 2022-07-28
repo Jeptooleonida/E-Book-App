980d04defa6853b24a4c4d1a371469879e565fd5
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-07-28 15:53:12 +0300

    add image on the header section

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,7 +5,8 @@ function Main(){
         < >
             <div className="header-section" >
                 <div className="logo-section">
-                     <img src = "https://thumbs.dreamstime.com/b/blurred-image-many-old-books-bookshelf-library-81867124.jpg"/> 
+                     <img src = "https://thumbs.dreamstime.com/b/blurred-image-many-old-books-bookshelf-library-81867124.jpg"/>
+ 
                 </div>
                 <div className="search-section">
                     <h2> Find your book here </h2>
