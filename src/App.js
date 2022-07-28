592f98e4533c7274109da5f73087f29148515d63
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-07-28 22:47:51 +0300

    add Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './Components/Main.js';
-import './Components/styles.css';
+// import Footer from './Components/Footer.js';
+import Navbar from './Components/Navbar';
 
 function App() {
   return(
     <>
+    <Navbar />
     <Main />
     </>
 
