c747a10b198d351680dba2265353316a95d218c9
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-08-02 17:51:47 +0300

    update API

--- src/API.js
+++ src/API.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://example-data.draftbit.com/books?_limit=50";
-export const BOOK_DETAILS_URL = "https://example-data.draftbit.com/books/";
+export const API_URL = "https://quiet-ocean-84468.herokuapp.com/books";
+export const BOOK_DETAILS_URL = "https://salty-inlet-00411.herokuapp.com/favorites";
