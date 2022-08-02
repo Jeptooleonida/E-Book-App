0716288d399e7275eb2f19f856a7be0e25a01d8b
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-08-02 13:31:08 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import './App.css';
-import Main from './Components/Main.js';
-import Navbar from './Components/Navbar.js';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import BookList from "./Components/BookList";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Main from "./Components/Main";
+import BookDetails from './Components/BookDetails';
+import Favorites from './Components/Favorites';
 
 function App() {
-  return(
-    < >
+  return (
+    <div className="App">
       <Navbar />
+      <Favorites />
       <Main />
-   </>
-  )
+    
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
