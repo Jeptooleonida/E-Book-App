5e65a8194b1b191c90f05bd43fb760e42b86f894
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-08-02 17:18:32 +0300

    Updates

---
+++ precache-manifest.e36c1e00196d5cb82eeac93b5c96c30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e55ea0cc5ca6194a0879",
+    "url": "/E-Book-App/static/css/main.deb197d0.chunk.css"
+  },
+  {
+    "revision": "e55ea0cc5ca6194a0879",
+    "url": "/E-Book-App/static/js/main.d695914c.chunk.js"
+  },
+  {
+    "revision": "7270d9e5d4af5fe9ed32",
+    "url": "/E-Book-App/static/js/runtime~main.bf240df5.js"
+  },
+  {
+    "revision": "befba6552d9c8c9fc966",
+    "url": "/E-Book-App/static/js/2.fe7f4731.chunk.js"
+  },
+  {
+    "revision": "50f4a02157d83cc5f2d4",
+    "url": "/E-Book-App/static/js/3.09a0fe09.chunk.js"
+  },
+  {
+    "revision": "ea1339f5f8926bcebf7c5f41acdeba08",
+    "url": "/E-Book-App/index.html"
+  }
+];
